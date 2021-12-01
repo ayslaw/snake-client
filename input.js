ae3a293f7d90b57f254b5b9319b7e7e7b1cf7bf7
ayslaw <ayoubf.chp@gmail.com>
2021-12-01 16:25:34 -0500

    sending movement commands

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,8 +13,39 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     // \u0003 = ctrl+c. if user inputs ctrl+c, exit the game
     process.exit();
+  
+  
   }
+
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  
+  if (key === 's') {
+    connection.write("Move: down");
+
+  }
+    if (key === 'a') {
+      connection.write("Move: left");
+
+    }
+      if (key === 'd') {
+        connection.write("Move: right");
+
+      }
+
 };
+
+
+
+
+// binding movements to commands 
+
+// up = 38;
+// down = 40
+// right = 39
+//left = 37
+
 
 
 module.exports = setupInput;
